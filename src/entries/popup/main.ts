2a0ae82a1928cdf166c8b5e079108c2b1e6d49ca
uncenter <47499684+uncenter@users.noreply.github.com>
2024-03-11 08:57:25 -0400

    feat(popup): improve styling

--- src/entries/popup/main.ts
+++ src/entries/popup/main.ts
@@ -1,17 +1,20 @@
+import './styles.css';
+
 import type { Flavor } from '@/lib/types';
 
 import { flavor } from '@/lib/storage';
 
 async function init() {
 	const flavorEl = document.querySelector('#flavor') as HTMLSelectElement;
+
 	flavorEl.value = await flavor.getValue();
-	flavorEl.addEventListener(
-		'change',
-		async ({ target }) =>
-			await flavor.setValue(
-				(target as HTMLSelectElement).value as Flavor,
-			),
-	);
+	document.documentElement.setAttribute('theme', flavorEl.value);
+
+	flavorEl.addEventListener('change', async ({ target }) => {
+		const value = (target as HTMLSelectElement).value as Flavor;
+		await flavor.setValue(value);
+		document.documentElement.setAttribute('theme', value);
+	});
 }
 
 init();
