bca89a64ae422245df93b448e9d6d82e1d1c8904
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-03 19:43:21 -0400

    chore: update comment

--- wxt.config.ts
+++ wxt.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 		name: 'Catppuccin for GitHub File Explorer Icons',
 		// See https://github.com/fregante/webext-dynamic-content-scripts/blob/main/how-to-add-github-enterprise-support-to-web-extensions.md.
 		permissions: ['storage', 'scripting', 'contextMenus', 'activeTab'],
-		// @ts-expect-error: Now in browsers. See https://github.com/w3c/webextensions/issues/119.
+		// @ts-expect-error: Now in browsers (see https://github.com/w3c/webextensions/issues/119).
 		optional_host_permissions: ['*://*/*'],
 		// Make sure to update in src/entries/content/index.ts as well.
 		content_scripts: [
