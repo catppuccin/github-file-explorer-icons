8727c1133cca955fe1e287db4879dc5276dab1b6
uncenter <uncenter@uncenter.dev>
2025-03-04 09:57:17 -0500

    feat: support tangled.sh [WIP]
    Removes SVGO / SVG optimization, and no longer removes the wrapper `<svg>` tag and attributes.

---
+++ src/sites/tangled.ts
@@ -0,0 +1,16 @@
+import type { ReplacementSelectorSet, Site } from '.';
+
+const mainRepositoryImplementation: ReplacementSelectorSet = {
+	row: '#file-tree > div, .tree > div',
+	filename: 'a',
+	icon: 'svg',
+	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
+		iconEl.classList.contains('lucide-folder'),
+	isSubmodule: (_rowEl, _fileNameEl, _iconEl) => false, // TODO: Implement isSubmodule.
+	isCollapsable: (_rowEl, _fileNameEl, _iconEl) => false,
+};
+
+export const tangled: Site = {
+	domains: ['tangled.sh'],
+	replacements: [mainRepositoryImplementation],
+};
